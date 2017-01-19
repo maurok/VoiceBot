53a84090a79d2b8ba1d6be3629af5a0ba7c8ab38
maurok <maurok@gmail.com>
2017-01-19 11:11:21 -0300

    # If 403 response then try again

--- EmergencyServicesBot/Services/MSCognitiveSpeechService.cs
+++ EmergencyServicesBot/Services/MSCognitiveSpeechService.cs
@@ -9,6 +9,8 @@
 
     public class MicrosoftCognitiveSpeechService
     {
+        private const string recognizeUri = "/recognize?scenarios=smd&appid=D4D52672-91D7-4C74-8AD8-42B1D98141A5&locale={0}&device.os=bot&form=BCSSTT&version=3.0&format=json&instanceid=565D69FF-E928-4B7E-87DA-9A750B96D9E3&requestid={1}";
+
         public string DefaultLocale { get; } = "en-US";
 
         /// <summary>
@@ -16,25 +18,36 @@
         /// </summary>
         /// <param name="audiostream"></param>
         /// <returns>Transcribed text. </returns>
-        public async Task<string> GetTextFromAudioAsync(Stream audiostream)
+        public async Task<string> GetTextFromAudioAsync(Stream audiostream, int retryCount = 0)
         {
-            var requestUri = string.Format(
-                "/recognize?scenarios=smd&appid=D4D52672-91D7-4C74-8AD8-42B1D98141A5&locale={0}&device.os=bot&form=BCSSTT&version=3.0&format=json&instanceid=565D69FF-E928-4B7E-87DA-9A750B96D9E3&requestid={1}",
-                DefaultLocale,
-                Guid.NewGuid());
+            // if we are retrying then refresh auth token, but do not retry more than once
+            if (retryCount > 0)
+            {
+                Authentication.Instance.GetAccessToken(true);
+
+                if (retryCount > 1)
+                {
+                    return string.Empty;
+                }
+            }
 
             using (var client = new HttpClient())
             {
                 client.BaseAddress = new Uri("https://speech.platform.bing.com");
-
-                var token = Authentication.Instance.GetAccessToken();
-                client.DefaultRequestHeaders.Add("Authorization", "Bearer " + token.access_token);
+                client.DefaultRequestHeaders.Add("Authorization", "Bearer " + Authentication.Instance.GetAccessToken());
 
                 try
                 {
                     using (var binaryContent = new ByteArrayContent(StreamToBytes(audiostream)))
                     {
+                        var requestUri = string.Format(recognizeUri, this.DefaultLocale, Guid.NewGuid());
+
                         var response = await client.PostAsync(requestUri, binaryContent);
+                        if (response.StatusCode == System.Net.HttpStatusCode.Forbidden)
+                        {
+                            return await GetTextFromAudioAsync(audiostream, ++retryCount);
+                        }
+
                         var responseString = await response.Content.ReadAsStringAsync();
                         dynamic data = JsonConvert.DeserializeObject(responseString);
 
